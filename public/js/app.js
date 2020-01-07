64bf28ff55a5c441ea6d7b98b03ce4d221a02e16
apex2060 <apex2060@gmail.com>
2020-01-07 10:28:23 -0700

    test messaging 2

--- public/js/app.js
+++ public/js/app.js
@@ -159,11 +159,14 @@ app.controller('SiteCtrl', function SiteCtrl($rootScope, $firebaseAuth, $firebas
 			init: function(user){
 				if(window.gtag)
 					gtag('set', {user_id: user.uid});
-				new Fire(`profile/${user.uid}`).get().then(profile=>{
-					$rootScope.profile = profile;
-					tools.profile.setup(profile);
-					tools.device.init();
-				})
+				if(!$scope.profile){
+					$scope.profile = {status:'pending'}
+					new Fire(`profile/${user.uid}`).get().then(profile=>{
+						$rootScope.profile = profile;
+						tools.profile.setup(profile);
+						tools.device.init();
+					})
+				}
 			},
 			// gapi: function(user){
 			// 	gapi.load('client', function(){
