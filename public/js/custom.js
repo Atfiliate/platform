7a0d80f7eb74c633a24248fc50c2702c5b2b6aec
apex2060 <apex2060@gmail.com>
2020-01-29 05:11:53 -0700

    test fix for ios v2

--- public/js/custom.js
+++ public/js/custom.js
@@ -53,14 +53,15 @@ String.prototype.toCamelCase = function() {
 	return str.replace(/\W/g, '')
 }
 String.prototype.hashCode = function() {
-	var hash = 0, i, chr;
-	if (this.length === 0) return hash;
-	for (i = 0; i < this.length; i++) {
-		chr   = this.charCodeAt(i);
-		hash  = ((hash << 5) - hash) + chr;
-		hash |= 0; // Convert to 32bit integer
-	}
-	return hash;
+	return '_test_'
+	// var hash = 0, i, chr;
+	// if (this.length === 0) return hash;
+	// for (i = 0; i < this.length; i++) {
+	// 	chr   = this.charCodeAt(i);
+	// 	hash  = ((hash << 5) - hash) + chr;
+	// 	hash |= 0; // Convert to 32bit integer
+	// }
+	// return hash;
 };
 
 
