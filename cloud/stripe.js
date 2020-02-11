93f5b87e50f27bbdbe8d4ba06d1fa409a22cb5b0
apex2060 <apex2060@gmail.com>
2020-02-11 13:58:08 -0700

    test stripe 4

--- cloud/stripe.js
+++ cloud/stripe.js
@@ -56,6 +56,7 @@ module.exports = {
 									stripe.products.create({
 										name: 		params.description,
 										type: 		'service',
+										statement_descriptor: params.description
 									}).then(product=>{
 										ref.set(product).then(()=>{
 											res(product)
