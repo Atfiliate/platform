0e5ee6bd705562a82c0ff8b589c427e94e1e9ecd
apex2060 <apex2060@gmail.com>
2020-04-06 13:52:59 -0600

    remove options and added default home path

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', function(request, response) {
 	response.render('pages/index');
 });
 
-app.options('/home/:path', auto.options)
+app.get('/home', auto.home)
 app.get('/home/:path', auto.home)
 
 app.get('/:root', auto.project)
