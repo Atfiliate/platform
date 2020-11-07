6d774c83c1f6b4ee71dba3e23e14dd9f6d332caa
Ryan Quinlan <apex2060@gmail.com>
2020-11-06 22:57:22 -0700

    test

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({limit:'50mb'}));
-// app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
-app.use(bodyParser.urlencoded());
-app.use(multer()); 
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+// app.use(bodyParser.urlencoded());
+app.use(multer().any());
 app.use(busboy());
 app.use(compression());
 
