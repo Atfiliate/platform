f360c383fa007fe888d9cb1dce948a2e2ac3454c
Ryan Quinlan <apex2060@gmail.com>
2020-11-06 22:39:07 -0700

    dangerous test modifying bodyParser

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json({limit:'50mb'}));
-app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+// app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
+app.use(bodyParser.urlencoded());
 app.use(busboy());
 app.use(compression());
 
