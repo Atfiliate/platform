6ff8d2950d8077d6dec7e95f3e07f99dbdf3df71
Ryan Quinlan <apex2060@gmail.com>
2021-12-03 19:13:28 -0600

    Test

--- cloud/fire.js
+++ cloud/fire.js
@@ -1,15 +1,14 @@
 var fs			= require('fs');
 var firebase	= require('firebase-admin');
 
-// console.log({
-// 	projectId: process.env.googleProject,
-// 	clientEmail: process.env.googleEmail,
-// 	privateKey: process.env.googleKey
-// })
-let dbUrl = process.env.config && process.env.config.firebase && process.env.config.firebase.databaseURL;
-if(dbUrl && process.env.googleJson)
+
+let databaseURL = process.env.config && process.env.config.firebase && process.env.config.firebase.databaseURL;
+console.log({databaseURL});
+
+if(databaseURL && process.env.googleJson){
 	firebase.initializeApp({
-		databaseURL: dbUrl,
+		databaseURL,
 		// credential: firebase.credential.cert(JSON.parse(fs.readFileSync('./cloud/service.json', 'utf8')))
 		credential: firebase.credential.cert(JSON.parse(process.env.googleJson))
 	});
+}
