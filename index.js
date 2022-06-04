107eef276244c3c8158af9bd18360abc5517c3da
Ryan Quinlan <apex2060@gmail.com>
2022-06-04 15:15:21 -0500

    adding ability to make http req for project page code

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ app.post('/cloud/:path/:id', auto.cloud)
 app.get('/cloud/:path/:id', auto.cloud)
 app.get('/component/:path', auto.component)
 
+app.get('/project/:projId', auto.options)
+app.get('/project/:projId', auto.project)
 app.get('/project/:projId/component/:component', auto.project)
 app.get('/project/component/:component', auto.project)
 
