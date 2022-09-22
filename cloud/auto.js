ef3721fc6ad532187b259d59b18930cab1e6e3ed
Ryan Quinlan <apex2060@gmail.com>
2022-09-22 15:59:38 -0500

    Update cloud code custom functions

--- cloud/auto.js
+++ cloud/auto.js
@@ -17,6 +17,105 @@ if(process.env.cloudinaryName)
 	});
 
 
+Array.prototype._flat = Array.prototype.flat;
+//fix because someone added a 'flat' to arrays in standards :S
+Array.prototype.flat = function(path, placeholder){
+	if(path)
+		return this.map(function(i){
+			let val = pathValue(i, path);
+			return i ? placeholder ? val === null ? placeholder : val : val : null;
+		})
+	else
+		return this._flat();
+}
+//Depricating getUnique. not sure where this is used outside of an old unique fn. which is no longer calling this.
+Array.prototype.getUnique = function() {
+	var u = {undefined:1},
+		a = [];
+	for (var i = 0, l = this.length; i < l; ++i) {
+		if (u.hasOwnProperty(this[i]))
+			continue;
+		a.push(this[i]);
+		u[this[i]] = 1;
+	}
+	return a;
+}
+Array.prototype.unique = function(path){
+	var u = [],
+		a = [];
+    this.forEach(i=>{
+        if(path){
+            let val = pathValue(i, path);
+            if(typeof val !== 'undefined' && !u.includes(val)){
+                u.push(val);
+                a.push(i)
+            }
+        }else{
+            if(typeof i !== 'undefined' && !a.includes(i))
+                a.push(i);
+        }
+    })
+    return a;
+}
+
+Array.prototype.shuffle = function() {
+	var i = this.length,
+		j, temp;
+	if (i == 0) return this;
+	while (--i) {
+		j = Math.floor(Math.random() * (i + 1));
+		temp = this[i];
+		this[i] = this[j];
+		this[j] = temp;
+	}
+	return this;
+}
+Array.prototype.max = function() {
+	return Math.max.apply(null, this);
+};
+
+Array.prototype.min = function() {
+	return Math.min.apply(null, this);
+};
+Array.prototype.allKeys = function(){
+	var keys = [];
+	this.forEach(function(obj){
+		if(typeof obj == 'object')
+			Object.keys(obj).forEach(function(k){
+				keys.push(k)
+			})
+	})
+	return keys.unique();
+}
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+String.prototype.toCamelCase = function() {
+	var str = this.replace(/\s(.)/g, function($1) { return $1.toUpperCase(); })
+		.replace(/\s/g, '')
+		.replace(/^(.)/, function($1) { return $1.toLowerCase(); })
+	return str.replace(/\W/g, '')
+}
+String.prototype.hashCode = function() {
+	var hash = 0, i, chr;
+	if (this.length === 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		chr   = this.charCodeAt(i);
+		hash  = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+};
+String.prototype.compile = function(scope){
+	let str = this;
+	let parts = str.split('}}');
+	return parts.map(p=>{
+		let p2 = p.split('{{');
+		if(p2[1])
+			p2[1] = pathValue(scope, p2[1]) || '';
+		return p2.join('');
+	}).join('');
+}
 
 function pathValue(obj, path, val){
     let parts, attr;
@@ -60,62 +159,6 @@ function pathValue(obj, path, val){
 		else
 			return obj;
 	}
-}
-
-Array.prototype._flat = Array.prototype.flat;
-//fix because someone added a 'flat' to arrays in standards :S:)
-Array.prototype.flat = function(col, placeholder){
-	if(col)
-		return this.map(function(i){
-			return i[col] && i[col] === undefined ? placeholder : i[col];
-		})
-	else
-		return this._flat();
-}
-Array.prototype.getUnique = function() {
-	var u = {undefined:1},
-		a = [];
-	for (var i = 0, l = this.length; i < l; ++i) {
-		if (u.hasOwnProperty(this[i]))
-			continue;
-		a.push(this[i]);
-		u[this[i]] = 1;
-	}
-	return a;
-}
-Array.prototype.unique = function(col){
-	if(col)
-		return this.flat(col).getUnique();
-	else
-		return this.getUnique();
-}
-Array.prototype.shuffle = function() {
-	var i = this.length,
-		j, temp;
-	if (i == 0) return this;
-	while (--i) {
-		j = Math.floor(Math.random() * (i + 1));
-		temp = this[i];
-		this[i] = this[j];
-		this[j] = temp;
-	}
-	return this;
-}
-Array.prototype.max = function() {
-	return Math.max.apply(null, this);
-};
-Array.prototype.min = function() {
-	return Math.min.apply(null, this);
-};
-String.prototype.compile = function(scope){
-	let str = this;
-	let parts = str.split('}}');
-	return parts.map(p=>{
-		let p2 = p.split('{{');
-		if(p2[1])
-			p2[1] = pathValue(scope, p2[1]) || '';
-		return p2.join('');
-	}).join('');
 }
 
 module.exports = {
