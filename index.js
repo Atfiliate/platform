29fdf3624a486ce7e4301eb483c7ce85dbb9a8ea
Ryan Quinlan <apex2060@gmail.com>
2023-04-15 14:18:13 -0500

    fix

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use((req, res, next)=>{
 	if(res.body.$fire || res.body[0] && res.body[0].$fire){
 		res.body = Fire.prepare(res.body);
 	}
+	next();
 })
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
