67a1f67977c3589ac327873e12e428096802b8fa
Ryan Quinlan <apex2060@gmail.com>
2023-04-15 14:42:54 -0500

    switch to reference other doc

--- index.js
+++ index.js
@@ -16,18 +16,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use((req, res, next)=>{
-	let origSend = res.send;
-	res.send = data=>{
-		console.log({data})
-		if((data && data.$fire) || (data && data[0] && data[0].$fire)){
-			data = Fire.prepare(data);
-		    res.setHeader('Content-Type', 'application/json');
-		}
-		origSend.call(res, data);
-	}
-	next();
-})
+app.use(auto.FireMiddleware)
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
 // app.use(bodyParser.urlencoded());
