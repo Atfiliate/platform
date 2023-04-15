dfc76e3a7e29356db479f609f10bf95f32d60474
Ryan Quinlan <apex2060@gmail.com>
2023-04-15 14:18:00 -0500

    attempt to make returning Fire objects to the client seemless.

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next)=>{
+	if(res.body.$fire || res.body[0] && res.body[0].$fire){
+		res.body = Fire.prepare(res.body);
+	}
+})
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
 // app.use(bodyParser.urlencoded());
