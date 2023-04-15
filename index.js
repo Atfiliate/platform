c7f528c4680fcfab3ba3844c8a159005a5a8ea86
Ryan Quinlan <apex2060@gmail.com>
2023-04-15 14:23:14 -0500

    test

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next)=>{
-	if(res.body.$fire || res.body[0] && res.body[0].$fire){
-		res.body = Fire.prepare(res.body);
-	}
+	console.log({res});
+	// if(res.body.$fire || res.body[0] && res.body[0].$fire){
+	// 	res.body = Fire.prepare(res.body);
+	// }
 	next();
 })
 app.use(bodyParser.json({limit:'50mb'}));
