d89c0294418e22f4289755ec7e52aba77775ab77
Ryan Quinlan <apex2060@gmail.com>
2023-05-09 13:36:36 -0500

    add jodit to public filesystem

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/jodit/build'));
 app.use(auto.FireMiddleware)
 app.use(bodyParser.json({limit:'50mb'}));
 app.use(bodyParser.urlencoded({extended:true, limit:'50mb'}));
