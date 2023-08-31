4cb719403327a53068bb510df0f400384cfd470c
Ryan Quinlan <apex2060@gmail.com>
2023-08-31 16:30:33 -0500

    fix firestore attempt to initialize without location.

--- cloud/fire.js
+++ cloud/fire.js
@@ -3,10 +3,14 @@ var firebase	= require('firebase-admin');
 
 if(process.env.config){
 	let config = JSON.parse(process.env.config);
-	firebase.initializeApp({
-		databaseURL: config.firebase.databaseURL,
-		// credential: firebase.credential.cert(JSON.parse(fs.readFileSync('./cloud/service.json', 'utf8')))
-		credential: firebase.credential.cert(JSON.parse(process.env.googleJson))
-	});
-	firebase.firestore().settings({ignoreUndefinedProperties:true});
+	if(config?.firebase?.databaseURL){
+		firebase.initializeApp({
+			databaseURL: config.firebase.databaseURL,
+			// credential: firebase.credential.cert(JSON.parse(fs.readFileSync('./cloud/service.json', 'utf8')))
+			credential: firebase.credential.cert(JSON.parse(process.env.googleJson))
+		});
+		firebase.firestore().settings({ignoreUndefinedProperties:true});
+	}else{
+		console.log('Firebase databaseURL not listed in config.')
+	}
 }
